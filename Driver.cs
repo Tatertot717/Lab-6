612a0a2aed4a15287e7ff92f1ec762cbbf1be88d
Tate J Smith <tatejsmith@bellsouth.net>
2023-02-15 12:38:06 -0500

    final

--- Driver.cs
+++ Driver.cs
@@ -1,7 +1,15 @@
-﻿class Driver
+﻿using System.Diagnostics.Metrics;
+
+class Driver
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        FibInteration a = new FibInteration();
+        FibFormula b = new FibFormula();
+        Console.WriteLine("Enter the number you want to find the Fibonacci Series for:") ;
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Fib of 10 by iteration is: "+a.CalculateFib(num));
+        Console.WriteLine("Fib of 10 by formula is: "+b.CalculateFib(num));
+
     }
 }
---
+++ FindFib.cs
@@ -0,0 +1,36 @@
+﻿using System;
+interface IFindFib 
+    {
+    int CalculateFib(int num);
+    }
+
+class FibInteration : IFindFib
+{
+    public int CalculateFib(int num)
+    {
+        if (num == 1 || num == 2)
+        {
+            return 1;
+        }
+        else {
+            int addthis = 1;
+            int addthis2 = 1;
+            int returnable = 1;
+            for (int i = 2; i < num; i++){
+                returnable = addthis + addthis2;
+                addthis2 = addthis;
+                addthis = returnable;
+                
+            }
+            return returnable;
+        }
+    }
+}
+
+class FibFormula : IFindFib
+{
+    public int CalculateFib(int num)
+    {
+        return (int)((Math.Pow(((1+Math.Sqrt(5))/2), num) - (Math.Pow(((1 - Math.Sqrt(5)) / 2), num)))/ Math.Sqrt(5));
+    }
+}
